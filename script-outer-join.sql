fbd15caffdb00316dc2505b583fe645735ce9ff7
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-21 01:19:34 -0300

    FULL JOIN

--- script-outer-join.sql
+++ script-outer-join.sql
@@ -1,1 +1,6 @@
-SELECT * FROM tb_cidades RIGHT JOIN tb_estados ON tb_estados.id_estados = tb_cidades.estado_id
+-- SELECT * FROM tb_cidades c LEFT JOIN tb_estados e on c.id_cidades = e.id_estados;
+-- SELECT * FROM tb_cidades c RIGHT JOIN tb_estados e on c.id_cidades = e.id_estados;
+-- SELECT * FROM tb_cidades c LEFT OUTER JOIN tb_estados e on c.estado_id = e.id_estados;
+SELECT * FROM tb_estados e LEFT OUTER JOIN tb_cidades c on c.estado_id = e.id_estados
+UNION 
+SELECT * FROM tb_estados e RIGHT JOIN tb_cidades c on c.estado_id = e.id_estados;
