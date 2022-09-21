061b35bb0fb44f09439ece5188de0248ac1a940e
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-21 00:42:44 -0300

    INSERT PREFEITOS

---
+++ script-insert-prefeito.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_prefeitos 
+    (nome_prefeito, cidade_id)
+VALUES
+    ('Dário Jorge Giolo Saadi', 1),
+    ('Axel Schmidt Grael', 2),
+    ('Rodrigo Pinheiro', 3);
--- script-join-tabelas.sql
+++ script-join-tabelas.sql
@@ -1,3 +1,9 @@
 SELECT 
   c.nome_cidade as 'NOME CIDADE', c.area_cidade AS 'ÁREA CIDADE', c.populacao_cidade AS 'POPULACAO', e.nome_estado AS 'ESTADO'
 FROM tb_estados e INNER JOIN tb_cidades c WHERE c.estado_id = e.id_estados
+
+-- Tipos de JOIN
+--  OUTER JOIN FORA DA INTERCESSÃO
+--  INNER JOIN INTERCESSÃO 
+--  LEFT JOIN TRAZ O QUE ESTA FORA DA INTERCESSÃO DO LADO ESQUERDO E A INTERCESSÃO
+--  RIGHT JOIN TRAZ O QUE FORA INTERCESSÃO DO LADO DIREITO E A INTERCESSÃO 
