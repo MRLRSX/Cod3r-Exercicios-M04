1acb9f32a501c862b40e95e304124955856e117d
MRLRSX <lucas.rocha-dev@outlook.com>
2022-09-20 22:57:28 -0300

    UP

--- escript-update-estados.sql
+++ escript-update-estados.sql
@@ -1,3 +1,7 @@
 -- UPDATE
 
 -- UPDATE tb_estados SET nome_estado = 'Maranhão' WHERE sigla_estado = 'MA';
+-- SELECT est.nome FROM tb_estados est where sigla_estado = 'MA'
+-- UPDATE tb_estados SET nome_estado = 'Paraná', populacao_estados = 11.32 WHERE sigla_estado = 'PR';
+
+SELECT est.nome_estado 'NOME', sigla_estado 'SIGLA', populacao_estados 'POPULAÇÃO' FROM tb_estados est WHERE sigla_estado = 'PR'
